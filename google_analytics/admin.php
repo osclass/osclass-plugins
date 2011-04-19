89917eb6832ae8bd8a38a892d452bfb4bda6c373
conejo-osclass <desteban@osclass.org>
2011-04-19 10:35:43 +0200

    small fixes on ads4osc plugin

--- google_analytics/admin.php
+++ google_analytics/admin.php
@@ -1,27 +1,28 @@
 <?php
 
     $dao_preference = new Preference() ;
-    if(isset($_REQUEST['webid'])) {
-        $webid = $_REQUEST['webid'];
-    } else {
-    	$webid = (osc_google_analytics_id()!='') ? osc_google_analytics_id() : '';
+    $webid          = Params::getParam('webid');
+    $option         = Params::getParam('option');
+    
+    if($webid == '') {
+    	$webid = (osc_google_analytics_id() != '') ? osc_google_analytics_id() : '';
     }
     
-    if(isset($_REQUEST['option']) && $_REQUEST['option']=='stepone') 
-    {
-        $dao_preference->update(array("s_value" => $webid), array("s_section" => "plugin-google_analytics", "s_name" => "google_analytics_id")) ;
-        echo '<div style="text-align:center; font-size:22px; background-color:#00bb00;"><p>Congratulations. The plugin is now configured.</p></div>' ;
+    if( $option == 'stepone' ) {
+        $dao_preference->update(array("s_value" => $webid)
+                               ,array("s_section" => "plugin-google_analytics", "s_name" => "google_analytics_id")) ;
+        echo '<div style="text-align:center; font-size:22px; background-color:#00bb00;"><p>' . __('Congratulations. The plugin is now configured','google_analytics') . '.</p></div>' ;
     }
 
 ?>
 
-<form action="<?php osc_admin_base_url(true); ?>" method="post">
+<form action="<?php osc_admin_base_url(true); ?>" method="get">
     <input type="hidden" name="page" value="plugins" />
     <input type="hidden" name="action" value="renderplugin" />
     <input type="hidden" name="file" value="google_analytics/admin.php" />
     <input type="hidden" name="option" value="stepone" />
     
     <div>
-        Please enter your Google Analytics <label for="id" style="font-weight: bold;">Web property ID</label>: <input type="text" name="webid" id="webid" value="<?php echo $webid; ?>" /> <input type="submit" value="Save" />
+        <?php _e('Please enter your Google Analytics', 'google_analytics'); ?> <label for="id" style="font-weight: bold;"><?php _e('Web property ID', 'google_analytics'); ?></label>: <input type="text" name="webid" id="webid" value="<?php echo $webid; ?>" /> <input type="submit" value="<?php _e('Save', 'google_analytics'); ?>" />
     </div>
 </form>
--- google_maps/map.php
+++ google_maps/map.php
@@ -1,40 +1,60 @@
-<script type="text/javascript">
-if(GBrowserIsCompatible()) {
-    var map = new GMap2(document.getElementById("itemMap"));
-    var geocoder = new GClientGeocoder();
-   map.addControl(new GSmallMapControl());
-    map.addControl(new GMapTypeControl());
-    var title_ad_googlemap = "<?= $item['s_title'] ?>";
-    var city_ad_googlemap = "<?= $item['s_city'] ?>";
-   var description_ad_googlemap = title_ad_googlemap + ', ' + city_ad_googlemap;
-   
-   
-    function showAddress(address, region) {
-        geocoder.getLatLng(
-            address,
-            function(point) {
-            if (!point) {
-                var geocoderRegion = new GClientGeocoder();
-                geocoderRegion.getLatLng(
-                    region,
+<div style="width: 100%; float:left; margin:20px 0 10px 60px"> 
+    <div id="itemMap"></div>
+</div>
+<?php if($item['d_coord_lat'] != '' && $item['d_coord_long'] != '') {?>
+    <script type="text/javascript">
+        if(GBrowserIsCompatible()) {
+            var map   = new GMap2(document.getElementById("itemMap"), { size: new GSize(480,240) });
+            var point = new GLatLng(<?php echo $item['d_coord_lat']; ?>, <?php echo $item['d_coord_long']; ?>);
+            map.setCenter(point, 13);
+            var marker = new GMarker(point);
+            map.addOverlay(marker);
+            marker.show();
+        }
+    </script>
+<?php } else { ?>
+    <script type="text/javascript"> 
+        var map = null;
+        var geocoder = null;
+     
+        if (GBrowserIsCompatible()) {
+            map = new GMap2(document.getElementById("itemMap"), { size: new GSize(480,240) });
+            map.setCenter(new GLatLng(37.4419, -122.1419), 13);
+            geocoder = new GClientGeocoder();
+        }
+     
+        function showAddress(address) {
+            if (geocoder) {
+                geocoder.getLatLng(
+                    address,
                     function(point) {
                         if (!point) {
-                            alert(region + " not found");
+                            //alert(address + " not found");
                         } else {
                             map.setCenter(point, 13);
                             var marker = new GMarker(point);
                             map.addOverlay(marker);
-                            //marker.openInfoWindowHtml(region);
+                            // As this is user-generated content, we display it as
+                            // text rather than HTML to reduce XSS vulnerabilities.
+                            marker.openInfoWindow(document.createTextNode(address));
                         }
                     }
                 );
-                } else {
-                    map.setCenter(point, 13);
-                    var marker = new GMarker(point);
-                    map.addOverlay(marker);
-                    marker.openInfoWindowHtml(description_ad_googlemap);
-                }
             }
-        );
-    }
-}
+        }
+        
+        <?php
+            $addr = array();
+            if($item['s_address']!='' && $item['s_address']!=null) { $addr[] = $item['s_address']; };
+            if($item['s_city']!='' && $item['s_city']!=null) { $addr[] = $item['s_city']; };
+            if($item['s_zip']!='' && $item['s_zip']!=null) { $addr[] = $item['s_zip']; };
+            if($item['s_region']!='' && $item['s_region']!=null) { $addr[] = $item['s_region']; };
+            if($item['s_country']!='' && $item['s_country']!=null) { $addr[] = $item['s_country']; };
+            $address = implode(", ", $addr);
+        ?>
+        $(document).ready(function(){
+            showAddress('<?php echo $address; ?>');
+        });
+
+    </script>
+<?php }; ?>
