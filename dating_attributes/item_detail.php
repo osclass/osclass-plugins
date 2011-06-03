a274075684f4d1d7ff0728503484e718795026c5
Juan Ramon <juanramon.diaz@gmail.com>
2011-06-03 18:24:04 +0200

    deleted other plugins

--- dating_attributes/item_detail.php
+++
@@ -1,3 +0,0 @@
-<h3><?php _e('Dating attributes', 'dating_attributes'); ?></h3>
-
-<h4><?php _e(sprintf('%s looking for %s for %s.', $detail['e_gender_from'], $detail['e_gender_to'], $detail['e_relation']), 'dating_attributes'); ?></h4>
