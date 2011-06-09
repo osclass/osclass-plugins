498863ffd78639add708f27113d1661098ecf89f
Juan Ramon <juanramon.diaz@gmail.com>
2011-06-09 13:34:15 +0200

    Now the master branch only contains the README file

--- facebook/struct.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE /*TABLE_PREFIX*/t_facebook_connect (
-    fk_i_user_id INT UNSIGNED NOT NULL,
-    i_facebook_uid VARCHAR(30) NULL,
-
-        PRIMARY KEY (fk_i_user_id),
-        FOREIGN KEY (fk_i_user_id) REFERENCES /*TABLE_PREFIX*/t_user (pk_i_id)
-) ENGINE=InnoDB DEFAULT CHARACTER SET 'UTF8' COLLATE 'UTF8_GENERAL_CI';
