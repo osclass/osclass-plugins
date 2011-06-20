0ae7c65698e0e5fbfd6070a10a1ea075a9dec422
Juan Ramon <juanramon.diaz@gmail.com>
2011-06-21 00:54:11 +0200

    youtube 0.9 version

---
+++ youtube/item_form.php
@@ -0,0 +1,9 @@
+<h2><?php _e('Youtube video', 'youtube'); ?></h2>
+<div class="box">
+    <div class="row">
+        <?php printf(__('Enter the youtube url, i.e.: <em>%s</em> or <em>%s</em>', 'youtube'), 'http://www.youtube.com/watch?v=ojqWclLQOxk', 'http://www.youtube.com/v/ojqWclLQOxk'); ?>
+    </div>
+    <div class="row" style="width: 500px;">
+        <input type="text" name="s_youtube" value="<?php echo @$detail['s_youtube']; ?>" />
+    </div>
+</div>
