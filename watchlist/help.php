fda2ff4c154c54393469bc53be21dbd539b1910b
unknown <keny10@gmail.com>
2011-07-11 15:19:59 -0400

    First release work only with OsClass v 2.2 and up

---
+++ watchlist/help.php
@@ -0,0 +1,60 @@
+<?php
+/*
+ *      OSCLass – software for creating and publishing online classified
+ *                           advertising platforms
+ *
+ *                        Copyright (C) 2010 OSCLASS
+ *
+ *       This program is free software: you can redistribute it and/or
+ *     modify it under the terms of the GNU Affero General Public License
+ *     as published by the Free Software Foundation, either version 3 of
+ *            the License, or (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful, but
+ *         WITHOUT ANY WARRANTY; without even the implied warranty of
+ *        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *             GNU Affero General Public License for more details.
+ *
+ *      You should have received a copy of the GNU Affero General Public
+ * License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+?>
+
+<div id="settings_form" style="border: 1px solid #ccc; background: #eee; ">
+    <div style="padding: 0 20px 20px;">
+        <div>
+            <fieldset>
+                <legend>
+                    <h1><?php _e('Watchlist Help', 'watchlist'); ?></h1>
+                </legend>
+                <h2>
+                    <?php _e('What is Watchlist Plugin?', 'watchlist'); ?>
+                </h2>
+                <p>
+                    <?php _e('Watchlist plugin allows you to display a link that will allow user to save items on a watchlist page.', 'watchlist'); ?>
+                </p>
+                <h2>
+                    <?php _e('How does Watchlist plugin work?', 'watchlist'); ?>
+                </h2>
+                <p>
+                    <?php _e('In order to use Watchlist plugin, you should edit your theme files and add the following line anywhere in the code you want the Watchlist link to appear', 'watchlist'); ?>:
+                </p>
+                <pre>
+                    &lt;?php watchlist(); ?&gt;
+                </pre>
+                <h2>
+                <?php _e('Could I cutomize the style of Watchlist plugin?', 'watchlist'); ?>
+                </h2>
+                <p>
+                    <?php _e("Of course you can. Watchlist display a link only you can use css to make a button or anything else.", 'watchlist'); ?>. 
+                </p>
+				<h2>
+                <?php _e('Did Watchlist plugin work with all version of Osclass ?', 'watchlist'); ?>
+                </h2>
+                <p>
+                    <?php _e("In order to work this pluggin need OsClass v2.2 and up without this version pluggin will crash. <br />You have OsClass ", 'watchlist'); ?> <?php echo OSCLASS_VERSION;?>. 
+                </p>
+              </fieldset>
+        </div>
+    </div>
+</div>
---
+++ watchlist/struct.sql
@@ -0,0 +1,7 @@
+CREATE TABLE /*TABLE_PREFIX*/t_item_watchlist (
+    id int(10) unsigned NOT NULL AUTO_INCREMENT,
+    fk_i_item_id INT(10) UNSIGNED,
+    fk_i_user_id INT(10) UNSIGNED,
+
+        PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARACTER SET 'UTF8' COLLATE 'UTF8_GENERAL_CI';
