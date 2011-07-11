216a52e35eaf0a1f8afcf691222928f1a196cdc9
conejoninja <conejo@theninjabunny.com>
2011-07-11 18:31:08 +0200

    plugin for PIWIK analytics

---
+++ piwik/conf.php
@@ -0,0 +1,54 @@
+<?php
+    /*
+     *      OSCLass – software for creating and publishing online classified
+     *                           advertising platforms
+     *
+     *                        Copyright (C) 2010 OSCLASS
+     *
+     *       This program is free software: you can redistribute it and/or
+     *     modify it under the terms of the GNU Affero General Public License
+     *     as published by the Free Software Foundation, either version 3 of
+     *            the License, or (at your option) any later version.
+     *
+     *     This program is distributed in the hope that it will be useful, but
+     *         WITHOUT ANY WARRANTY; without even the implied warranty of
+     *        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+     *             GNU Affero General Public License for more details.
+     *
+     *      You should have received a copy of the GNU Affero General Public
+     * License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+     */
+
+    if(Params::getParam('plugin_action')=='done') {
+        osc_set_preference('js_code', Params::getParam('js_code'), 'piwik', 'STRING');
+        echo '<div style="text-align:center; font-size:22px; background-color:#00bb00;"><p>' . __('Congratulations. The plugin is now configured', 'piwik') . '.</p></div>' ;
+        osc_reset_preferences();
+    }
+?>
+<div id="settings_form" style="border: 1px solid #ccc; background: #eee; ">
+    <div style="padding: 20px;">
+        <div style="float: left; width: 100%;">
+            <fieldset>
+                <legend><?php _e('Piwik Settings', 'piwik'); ?></legend>
+                <form name="piwik_form" id="piwik_form" action="<?php echo osc_admin_base_url(true); ?>" method="POST" enctype="multipart/form-data" >
+                    <div style="float: left; width: 100%;">
+                    <input type="hidden" name="page" value="plugins" />
+                    <input type="hidden" name="action" value="renderplugin" />
+                    <input type="hidden" name="file" value="<?php echo osc_plugin_folder(__FILE__); ?>conf.php" />
+                    <input type="hidden" name="plugin_action" value="done" />
+                        <label for="js_code"><?php _e('Write here the JS code for Piwik Analytics', 'piwik'); ?></label>
+                        <br/>
+                        <textarea name="js_code" id="js_code" rows="10" style="width:600px"><?php echo osc_get_preference('js_code', 'piwik'); ?></textarea>
+                        <br/>
+                        <?php _e("To get the JS code, you should first install piwik on your server. More information on http://piwik.org/",'piwik'); ?>
+                        <br/>
+                        <span style="float:right;"><button type="submit" style="float: right;"><?php _e('Update', 'piwik');?></button></span>
+                    </div>
+                    <br/>
+                    <div style="clear:both;"></div>
+                </form>
+            </fieldset>
+        </div>
+        <div style="clear: both;"></div>										
+    </div>
+</div>
---
+++ piwik/index.php
@@ -0,0 +1,42 @@
+<?php
+/*
+Plugin Name: Piwik Web Analytics
+Plugin URI: http://www.osclass.org/
+Description: Enable Piwik Web Analytics
+Version: 0.9
+Author: OSClass
+Author URI: http://www.osclass.org/
+Short Name: piwik
+*/
+
+
+    function piwik_install() {
+        osc_set_preference('js_code', '', 'piwik', 'STRING');
+    }
+
+    function piwik_uninstall() {
+        osc_delete_preference('js_code', 'piwik');
+    }
+    
+    function piwik_footer() {
+        echo osc_get_preference('js_code', 'piwik');
+    }
+
+    function piwik_admin_menu() {
+        echo '<h3><a href="#">Piwik Analytics</a></h3>
+        <ul> 
+            <li><a href="' . osc_admin_render_plugin_url(osc_plugin_folder(__FILE__) . 'conf.php') . '">&raquo; ' . __('Settings', 'piwik') . '</a></li>
+        </ul>';
+    }
+
+
+    /**
+     * ADD HOOKS
+     */
+    osc_register_plugin(osc_plugin_path(__FILE__), 'piwik_install');
+    osc_add_hook(osc_plugin_path(__FILE__)."_uninstall", 'piwik_uninstall');
+
+    osc_add_hook('admin_menu', 'piwik_admin_menu');
+    osc_add_hook('footer', 'piwik_footer');
+    
+?>
