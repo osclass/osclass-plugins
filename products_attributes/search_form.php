4826daf62526e4484754426e23998bfd8229fad7
Carlos <garciademarina@gmail.com>
2011-07-29 11:54:15 +0200

    fixed layout search_form

--- products_attributes/search_form.php
+++ products_attributes/search_form.php
@@ -1,16 +1,18 @@
 <h3><?php _e('Products attributes', 'products_attributes') ; ?></h3>
-<div>
+<div class="row one_input">
     <table>
         <tr>
             <td>
                 <label for="make"><?php _e('Make', 'products_attributes'); ?></label>
-                <input type="text" id="make" name="make" style="border:0; color:#f6931f; font-weight:bold;" />
+                <br/>
+                <input type="text" id="make" name="make" />
             </td>
         </tr>
         <tr>
             <td>
                 <label for="model"><?php _e('Model', 'products_attributes'); ?></label>
-                <input type="text" id="model" name="model" style="border:0; color:#f6931f; font-weight:bold;" />
+                <br/>
+                <input type="text" id="model" name="model" />
             </td>
         </tr>
     </table>
