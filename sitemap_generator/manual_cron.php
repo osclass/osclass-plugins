25d3ba4b79e0a21637d735328e12b9c07251b455
Juan Ramón <juanramon.diaz@gmail.com>
2012-02-06 10:22:39 +0100

    set ABS_PATH in sitemap manual cron

--- sitemap_generator/manual_cron.php
+++ sitemap_generator/manual_cron.php
@@ -1,8 +1,10 @@
 <?php
 
-    require_once('../../../oc-load.php');
-    require_once('index.php');
-    
+    define( 'ABS_PATH', dirname( dirname( dirname( dirname( __FILE__ ) ) ) ) . '/' ) ;
+
+    require_once( ABS_PATH . 'oc-load.php' ) ;
+    require_once( osc_content_path() . 'plugins/sitemap_generator/index.php' ) ;
+
     sitemap_generator();
 
 ?>
