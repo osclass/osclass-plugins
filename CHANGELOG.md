e13049715f0a0ce61603e6059fb492eceb92e418
Juan Ramón <juanramon.diaz@gmail.com>
2012-02-20 20:36:01 +0100

    version 1.0 WebRupee plugin

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+Changelog WebRupee
+==================
+
+##version 1.0 - 20/02/2012
+
+* First version of WebRupee plugin
---
+++ webrupee/index.php
@@ -0,0 +1,37 @@
+<?php
+/*
+Plugin Name: Replace Indian currency
+Plugin URI: http://www.osclass.org/
+Description: This plugin implements webrupee.com web API
+Version: 1.0
+Author: OSClass
+Author URI: http://www.osclass.org/
+Short Name: webrupee
+Plugin update URI: 
+*/
+
+    function webrupee_css() {
+        echo '<!-- WebRupee plugin -->' ;
+        echo '<link rel="stylesheet" type="text/css" href="http://cdn.webrupee.com/font">' ;
+        echo '<!-- /WebRupee plugin -->' ;
+    }
+
+    function webrupee_price($price) {
+        $webrupee = '<span class="WebRupee">Rs.</span>' ;
+        if( osc_theme() == 'modern' ) {
+            $webrupee = '<span class="WebRupee" style="border:0 ; margin:0 ; padding: 0;">Rs.</span>' ;
+        }
+
+        $price = preg_replace('|RS\.|i', $webrupee, $price) ;
+        $price = preg_replace('|INR|i', $webrupee, $price) ;
+
+        return $price ;
+    }
+
+    // add CSS from WebRupee in the header
+    osc_add_hook( 'header', 'webrupee_css' ) ;
+    
+    osc_add_filter( 'item_price', 'webrupee_price' ) ;
+
+    /* file end: ./webrupee/index.php */
+?>
