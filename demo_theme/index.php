caf97fa199eca8dff72a16702b8eee174f404f84
Juan Ramón <juanramon.diaz@gmail.com>
2012-03-19 18:14:10 +0100

    save selected theme in the cookie; v1.2

--- demo_theme/index.php
+++ demo_theme/index.php
@@ -3,56 +3,71 @@
 Plugin Name: Demo theme
 Plugin URI: http://www.osclass.org/
 Description: Rewrite all the urls adding the parameter theme. In addition, it loads the theme passed in the url as parameter. Ideal for showing different themes.
-Version: 1.1
+Version: 1.2
 Author: OSClass
 Author URI: http://www.osclass.org/
 Short Name: demo_theme
 */
 
     function change_theme() {
-        if( Params::getParam('theme') == '' ) {
+        $theme = '' ;
+        // check first if it has been set in the cookie
+        if( Cookie::newInstance()->get_value('demo_theme') != '' ) {
+            $theme = Cookie::newInstance()->get_value('demo_theme') ;
+        }
+
+        if( Params::getParam('theme') != '' ) {
+            $theme = Params::getParam('theme') ;
+            Cookie::newInstance()->set_expires( 86400 * 30 ) ;
+            Cookie::newInstance()->push('demo_theme', $theme) ;
+            Cookie::newInstance()->set() ;
+        }
+
+        if( $theme == '' ) {
             return false ;
         }
 
-        $theme_path = osc_themes_path() . Params::getParam('theme') . '/';
+        $theme_path = osc_themes_path() . $theme . '/';
         if( file_exists($theme_path) ) {
-            WebThemes::newInstance()->setCurrentTheme( Params::getParam('theme') ) ;
+            WebThemes::newInstance()->setCurrentTheme($theme) ;
             $functions_path = WebThemes::newInstance()->getCurrentThemePath() . 'functions.php';
             if ( file_exists($functions_path) ) {
-                require_once $functions_path;
+                require_once $functions_path ;
             }
         }
 
         return true ;
     }
 
-    function urls_theme_parameter() {
-        if( ( Params::getParam('theme') != '' ) && ( Params::getParam('theme') != osc_theme() ) ) {
-            $theme = Params::getParam('theme') ;
-            $js    = <<<JAVASCRIPT
-<script type="text/javascript">
-    var theme = '$theme' ;
-    \$.each(\$("a"), function (index, value) {
-        if(/\?/.test(value) ) {
-            \$(this).attr('href', $(this).attr('href') + "&theme=" + theme);
-        } else {
-            \$(this).attr('href', $(this).attr('href') + "?theme=" + theme);
-        }
-    });
-</script>
-
-JAVASCRIPT;
-            echo $js ;
-        }
-    }
-
+    /**
+     * 
+     */
     function theme_selector_css() {
         echo '<link href="' . osc_base_url() . 'oc-content/plugins/demo_theme/custom.css" media="screen" rel="stylesheet" type="text/css" />' ;
     }
 
+    /**
+     * 
+     * 
+     * @return string The name of the selected theme
+     */
+    function selected_theme() {
+        $theme = osc_theme() ;
+
+        if( Cookie::newInstance()->get_value('demo_theme') != '' ) {
+            $theme = Cookie::newInstance()->get_value('demo_theme') ;
+        }
+
+        if( Params::getParam('theme') != '' ) {
+            $theme = Params::getParam('theme') ;
+        }
+
+        return $theme ;
+    }
+
     function theme_selector_top() {
         $themes         = array();
-        $selected_theme = ( Params::getParam("theme") != '' ) ? Params::getParam("theme") : osc_theme( ) ;
+        $selected_theme = selected_theme() ;
         $aThemes        = WebThemes::newInstance()->getListThemes();
         foreach($aThemes as $theme) {
             $theme_info = WebThemes::newInstance()->loadThemeInfo($theme) ;
@@ -61,11 +76,14 @@ JAVASCRIPT;
 
         echo '<script type="text/javascript">' . PHP_EOL ;
         echo '    $(document).ready(function () {' . PHP_EOL ;
-        echo '        var theme = {' ;
-        foreach($themes as $t) {
-            echo '        ' . $t['theme'] . ': "' . $t['name'] . '", ' ;
+        echo '        var theme = { ' ;
+        for($i = 0; $i < count($themes); $i++) {
+            echo $themes[$i]['theme'] . ': "' . $themes[$i]['name'] . '"' ;
+            if( $i < (count($themes) - 1) ) {
+                echo ', ' ;
+            }
         }
-        echo '        } ;' . PHP_EOL;
+        echo ' } ;' . PHP_EOL;
         echo '        $("body").prepend( $("<div>").attr("id", "theme_header") ) ;' . PHP_EOL ;
         echo '        $("#theme_header").append( $("<div>").attr("id", "theme_selector") ) ;' . PHP_EOL ;
         echo '        $("#theme_selector").append( $("<label>").html("' . __('Choose a theme', 'demo_theme') . '") ) ;' . PHP_EOL ;
@@ -93,6 +111,5 @@ JAVASCRIPT;
 
     osc_add_hook('header', 'theme_selector_css') ;
 
-    osc_add_hook('footer', 'urls_theme_parameter') ;
     osc_add_hook('footer', 'theme_selector_top') ;
 ?>
