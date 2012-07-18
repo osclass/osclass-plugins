b54c97dd8f8f9b3abfadcede0bfb3092dbd6dba5
Carlos <garciademarina@gmail.com>
2012-07-18 13:58:59 +0200

    remove db connections and replace it using ModelVoting, add new MenuAdmin if version > 300, improved function voting_item_detail_user accept user id as first argument

--- voting/index.php
+++ voting/index.php
@@ -3,37 +3,27 @@
 Plugin Name: Voting
 Plugin URI: http://www.osclass.org/
 Description: Voting system
-Version: 1.0.1
+Version: 1.2
 Author: OSClass
 Author URI: http://www.osclass.org/
 Short Name: voting
 Plugin update URI: voting
 */
 
+    require_once 'ModelVoting.php' ;
+
     /**
      * Set plugin preferences 
      */
     function voting_install() 
     {
-        $conn = getConnection();
-        $conn->autocommit(false) ;
-        try {
-            $path = osc_plugin_resource('voting/struct.sql');
-            $sql = file_get_contents($path);
-            $conn->osc_dbImportSQL($sql);
-            // vote items
-            osc_set_preference('item_voting', '1', 'voting', 'BOOLEAN');
-            osc_set_preference('open', '1', 'voting', 'BOOLEAN');
-            osc_set_preference('user', '0', 'voting', 'BOOLEAN');
-            // vote users
-            osc_set_preference('user_voting', '0', 'voting', 'BOOLEAN');
-            
-            $conn->commit();
-        } catch (Exception $e) {
-            $conn->rollback();
-            echo $e->getMessage();
-        }
-        $conn->autocommit(true);
+        ModelVoting::newInstance()->import('voting/struct.sql');
+        // vote items
+        osc_set_preference('item_voting', '1', 'voting', 'BOOLEAN');
+        osc_set_preference('open', '1', 'voting', 'BOOLEAN');
+        // vote users
+        osc_set_preference('user', '0', 'voting', 'BOOLEAN');
+        osc_set_preference('user_voting', '0', 'voting', 'BOOLEAN');
     }
 
     /**
@@ -41,25 +31,13 @@ Plugin update URI: voting
      */
     function voting_uninstall() 
     {
-        $conn = getConnection();
-        $conn->autocommit(false);
-        
-        try {
-            $conn->osc_dbExec("DELETE FROM %st_plugin_category WHERE s_plugin_name = 'voting'", DB_TABLE_PREFIX);
-            $conn->osc_dbExec('DROP TABLE %st_voting_item', DB_TABLE_PREFIX);
-            $conn->osc_dbExec('DROP TABLE %st_voting_user', DB_TABLE_PREFIX);
-            
-            osc_delete_preference('item_voting', 'voting');
-            osc_delete_preference('open', 'voting');
-            osc_delete_preference('user', 'voting');
-            osc_delete_preference('user_voting', 'voting');
-            
-            $conn->commit();
-        } catch (Exception $e) {
-            $conn->rollback();
-            echo $e->getMessage();
-        }
-        $conn->autocommit(true);
+        ModelVoting::newInstance()->uninstall();
+        // vote items
+        osc_delete_preference('item_voting', 'voting');
+        osc_delete_preference('open', 'voting');
+        // vote users
+        osc_delete_preference('user', 'voting');
+        osc_delete_preference('user_voting', 'voting');
     }
     
     /**
@@ -67,11 +45,14 @@ Plugin update URI: voting
      */
     function voting_admin_menu() 
     {
-        echo '<h3><a href="#">' . __('Voting options', 'voting') . '</a></h3>
-        <ul> 
-            <li><a href="' . osc_admin_render_plugin_url(osc_plugin_folder(__FILE__) . 'conf.php') . '">&raquo; ' . __('Settings', 'voting') . '</a></li>
-            <li><a href="' . osc_admin_render_plugin_url(osc_plugin_folder(__FILE__) . 'help.php') . '">&raquo; ' . __('Help', 'voting') . '</a></li>
-        </ul>';
+        // check if osc_version is greather than 3.0
+        if(osc_version()<300) {
+            echo '<h3><a href="#">' . __('Voting options', 'voting') . '</a></h3>
+            <ul> 
+                <li><a href="' . osc_admin_render_plugin_url(osc_plugin_folder(__FILE__) . 'conf.php') . '">&raquo; ' . __('Settings', 'voting') . '</a></li>
+                <li><a href="' . osc_admin_render_plugin_url(osc_plugin_folder(__FILE__) . 'help.php') . '">&raquo; ' . __('Help', 'voting') . '</a></li>
+            </ul>';
+        }
     }
     
     function voting_admin_configuration() 
@@ -90,9 +71,9 @@ Plugin update URI: voting
     function voting_item_detail()
     {
         if (osc_is_this_category('voting', osc_item_category_id()) && osc_get_preference('item_voting', 'voting') == '1' ) {
-            $conn = getConnection();
-            $aux_vote  = $conn->osc_dbFetchResult('SELECT format(avg(i_vote),1) as vote FROM %st_voting_item WHERE fk_i_item_id = %s', DB_TABLE_PREFIX, osc_item_id());
-            $aux_count = $conn->osc_dbFetchResult('SELECT count(*) as total FROM %st_voting_item WHERE fk_i_item_id = %s', DB_TABLE_PREFIX, osc_item_id());
+            
+            $aux_vote  = ModelVoting::newInstance()->getItemAvgRating( osc_item_id() );
+            $aux_count = ModelVoting::newInstance()->getItemNumberOfVotes( osc_item_id() );
             $vote['vote']  = $aux_vote['vote'];
             $vote['total'] = $aux_count['total']; 
             
@@ -129,11 +110,10 @@ Plugin update URI: voting
      */
     function can_vote($itemId, $userId, $hash)
     {
-        $conn = getConnection();
         if( $userId == 'NULL' ) {
-            $result = $conn->osc_dbFetchResult("SELECT i_vote FROM %st_voting_item WHERE fk_i_item_id = %s AND fk_i_user_id IS NULL AND s_hash = '%s'", DB_TABLE_PREFIX, $itemId, $hash);
+            $result = Modelvoting::newInstance()->getItemIsRated($itemId, $hash);
         } else {
-            $result = $conn->osc_dbFetchResult("SELECT i_vote FROM %st_voting_item WHERE fk_i_item_id = %s AND fk_i_user_id = %s AND s_hash = '%s'", DB_TABLE_PREFIX, $itemId, $userId, $hash);
+            $result = Modelvoting::newInstance()->getItemIsRated($itemId, $hash, $userId);
         }
         
         if( count($result) > 0 ) 
@@ -177,7 +157,7 @@ Plugin update URI: voting
     function get_votes($array_filters)
     {
         $category_id = null;
-        $order       = null;
+        $order       = 'desc';
         $num         = 5;
         if(isset($array_filters['category_id'])){
             $category_id = $array_filters['category_id'];
@@ -192,38 +172,15 @@ Plugin update URI: voting
             $num = (int)$array_filters['num_items'];
         }
         
-        $sql  = 'SELECT fk_i_item_id as item_id, format(avg(i_vote),1) as avg_vote, count(*) as num_votes, '.DB_TABLE_PREFIX.'t_item.fk_i_category_id as category_id ';
-        if(!is_null($category_id)) {
-            $sql .= ', '.DB_TABLE_PREFIX.'t_category.fk_i_parent_id as parent_category_id ';
-        }
-        $sql .= 'FROM '.DB_TABLE_PREFIX.'t_voting_item ';
-        $sql .= 'LEFT JOIN '.DB_TABLE_PREFIX.'t_item ON '.DB_TABLE_PREFIX.'t_item.pk_i_id = '.DB_TABLE_PREFIX.'t_voting_item.fk_i_item_id ';
-        $sql .= 'LEFT JOIN '.DB_TABLE_PREFIX.'t_category ON '.DB_TABLE_PREFIX.'t_category.pk_i_id = '.DB_TABLE_PREFIX.'t_item.fk_i_category_id ';
-        if(!is_null($category_id)) {
-            $sql .= 'WHERE '.DB_TABLE_PREFIX.'t_item.fk_i_category_id = '.$category_id.' ';
-            $sql .= 'OR '.DB_TABLE_PREFIX.'t_category.fk_i_parent_id = '.$category_id.' ';
-            $sql .= ' AND ';
-        }else{
-            $sql .= 'WHERE ';
-        }
-        $sql .= ''.DB_TABLE_PREFIX.'t_item.b_active = 1 ';
-        $sql .= 'AND '.DB_TABLE_PREFIX.'t_item.b_enabled = 1 ';
-        $sql .= 'AND '.DB_TABLE_PREFIX.'t_item.b_spam = 0 ';
-        $sql .= 'AND ('.DB_TABLE_PREFIX.'t_item.b_premium = 1 || '.DB_TABLE_PREFIX.'t_category.i_expiration_days = 0 ||DATEDIFF(\''.date('Y-m-d H:i:s').'\','.DB_TABLE_PREFIX.'t_item.dt_pub_date) < '.DB_TABLE_PREFIX.'t_category.i_expiration_days) ';
-        $sql .= 'AND '.DB_TABLE_PREFIX.'t_category.b_enabled = 1 ';
-        $sql .= 'GROUP BY item_id ORDER BY avg_vote '.$order.', num_votes '.$order.' LIMIT 0, '.$num;
-        
-        $conn = getConnection();
-        return $conn->osc_dbFetchResults($sql);
+       return ModelVoting::newInstance()->getItemRatings($category_id, $order, $num);
     }
     
     /**
      * hook delete_item
      * @param type $itemID 
      */
-    function voting_item_delete($itemID) {
-        $conn = getConnection();
-        $conn->osc_dbExec("DELETE FROM %st_voting_item WHERE fk_i_item_id = '%d'", DB_TABLE_PREFIX, $itemID);
+    function voting_item_delete($itemId) {
+        return ModelVoting::newInstance()->deleteItem($itemId);
     }
     
     /**************************************************************************
@@ -233,48 +190,50 @@ Plugin update URI: voting
     /**
      * Show form to vote a seller if item belongs to a registered user. (itemDetail)
      */
-    function voting_item_detail_user()
+    function voting_item_detail_user($userId)
     {
-        $userId = osc_item_user_id() ;
-        if(osc_get_preference('user_voting', 'voting') == 1 && is_numeric($userId) && isset($userId) ) {            
-            if( $userId ) {
-                // obtener el avg de las votaciones
-                $conn = getConnection();
-                $aux_vote  = $conn->osc_dbFetchResult('SELECT format(avg(i_vote),1) as vote FROM %st_voting_user WHERE i_user_voted = %s', DB_TABLE_PREFIX, $userId);
-                $aux_count = $conn->osc_dbFetchResult('SELECT count(*) as total FROM %st_voting_user WHERE i_user_voted = %s', DB_TABLE_PREFIX, $userId);
-                $vote['vote']  = $aux_vote['vote'];
-                $vote['total'] = $aux_count['total']; 
-          
-                $vote['can_vote'] = true;
-                if( !osc_is_web_user_logged_in() || !can_vote_user(osc_item_user_id(), osc_logged_user_id()) ){
-                    $vote['can_vote'] = false;
-                }
+        if( is_array($userId) ) {
+            $userId = osc_item_user_id();
+        } else if($userId == null && !is_numeric($userId) ) {
+            exit;
+        }
+        
+        if( osc_get_preference('user_voting', 'voting') == 1 && is_numeric($userId) && isset($userId) && $userId > 0) {
+            // obtener el avg de las votaciones
+            $aux_vote  = ModelVoting::newInstance()->getUserAvgRating($userId);
+            $aux_count = ModelVoting::newInstance()->getUserNumberOfVotes($userId);
+            $vote['vote']   = $aux_vote['vote'];
+            $vote['total']  = $aux_count['total']; 
+            $vote['userId'] = $userId;
 
-                require 'item_detail_user.php';
+            $vote['can_vote'] = false;
+            if(osc_is_web_user_logged_in() && can_vote_user($userId, osc_logged_user_id()) ) {
+                $vote['can_vote'] = true;
             }
+            require 'item_detail_user.php';
         }
     }
     
     /**
      * Check if user can vote
      *
-     * @param string $itemId
-     * @param string $userId
-     * @param string $hash
-     * @return bool
+     * @param type $userVotedId
+     * @param type $userId
+     * @return type 
      */
     function can_vote_user($userVotedId, $userId)
     {
-        $conn   = getConnection();
-        $result = 0;
+        $result = array();
         if( isset($userVotedId) && is_numeric($userVotedId) && isset($userId) && is_numeric($userId) && $userId != $userVotedId) {
-            $result = $conn->osc_dbFetchResult("SELECT i_vote FROM %st_voting_user WHERE i_user_voted = %s AND i_user_voter = %s", DB_TABLE_PREFIX, $userVotedId, $userId);
-        }
-        
-        if( count($result) > 0 ) 
+            $result = ModelVoting::newInstance()->getUserIsRated($userVotedId, $userId);
+            if( count($result) > 0 ) {
+                return false;
+            } else {
+                return true;   
+            }
+        } else {
             return false;
-        else 
-            return true;   
+        }
     }
     
     /**
@@ -310,7 +269,7 @@ Plugin update URI: voting
      */
     function get_user_votes($array_filters)
     {
-        $order       = null;
+        $order       = 'desc';
         $num         = 5;
         if(isset($array_filters['order'])){
             $order = strtolower($array_filters['order']);
@@ -322,26 +281,16 @@ Plugin update URI: voting
             $num = (int)$array_filters['num_items'];
         }
         
-        $sql  = 'SELECT i_user_voted as user_id, format(avg(i_vote),1) as avg_vote, count(*) as num_votes ';
-        $sql .= 'FROM '.DB_TABLE_PREFIX.'t_voting_user ';
-        $sql .= 'LEFT JOIN '.DB_TABLE_PREFIX.'t_user ON '.DB_TABLE_PREFIX.'t_user.pk_i_id = '.DB_TABLE_PREFIX.'t_voting_user.i_user_voted ';
-        $sql .= 'WHERE ';
-        $sql .= ''.DB_TABLE_PREFIX.'t_user.b_active = 1 ';
-        $sql .= 'AND '.DB_TABLE_PREFIX.'t_user.b_enabled = 1 ';
-        $sql .= 'GROUP BY user_id ORDER BY avg_vote '.$order.', num_votes '.$order.' LIMIT 0, '.$num;
-        
-        $conn = getConnection();
-        return $conn->osc_dbFetchResults($sql);
+       return ModelVoting::newInstance()->getUserRatings($order, $num);
     }
     
     /**
      * hook delete
      * @param type $userID 
      */
-    function voting_user_delete($userID) {
-        $conn = getConnection();
-        $conn->osc_dbExec("DELETE FROM %st_voting_user WHERE i_user_voted = %d", DB_TABLE_PREFIX, $userID);
-        $conn->osc_dbExec("DELETE FROM %st_voting_user WHERE i_user_voter = %d", DB_TABLE_PREFIX, $userID);
+    function voting_user_delete($userId) 
+    {
+        ModelVoting::newInstance()->deleteUser($userId);
     }
     
     /**
@@ -351,7 +300,8 @@ Plugin update URI: voting
      * @param type $avg_vote
      * @return type 
      */
-    function voting_star($star, $avg_vote){
+    function voting_star($star, $avg_vote)
+    {
         $path = osc_base_url().'/oc-content/plugins/'.  osc_plugin_folder(__FILE__);
         $star_ok = $path.'img/ico_vot_ok.gif';
         $star_no = $path.'img/ico_vot_no.gif';
@@ -390,5 +340,40 @@ Plugin update URI: voting
     osc_add_hook('delete_item', 'voting_item_delete');
     osc_add_hook('delete_user', 'voting_user_delete');
 
+     /**
+     * ADMIN MENU
+     */
+    if(osc_version() >= 300) {
+        AdminMenu::newInstance()->add_menu(__('Voting options', 'voting'), osc_admin_render_plugin_url(osc_plugin_folder(__FILE__) . 'conf.php'), 'voting_plugin', 'administrator') ; 
+        AdminMenu::newInstance()->add_submenu( 'voting_plugin', __('Settings', 'voting'), osc_admin_render_plugin_url(osc_plugin_folder(__FILE__) . 'conf.php'), 'voting_plugin_settings', 'administrator') ;
+        AdminMenu::newInstance()->add_submenu( 'voting_plugin', __('Help', 'voting'), osc_admin_render_plugin_url(osc_plugin_folder(__FILE__) . 'help.php'), 'voting_plugin_help', 'administrator') ;
+    }
+    
+    function votingmenu() { ?>
+<style>
+    .ico-voting_plugin {
+        background-image: url('<?php echo osc_base_url();?>oc-content/plugins/<?php echo osc_plugin_folder(__FILE__);?>img/split.png') !important;
+        background-position:0px -48px;
+    }
+    .ico-voting_plugin:hover,
+    .current .ico-voting_plugin{
+        background-position:0px -0px;
+    }
+    
+    
+    
+    
+    body.compact .ico-voting_plugin{
+            background-position:-48px -48px;
+    }
+    body.compact .ico-voting_plugin:hover,
+    body.compact .current .ico-voting_plugin{
+        background-position:-48px 0px;
+    }
+</style>
+    <?php
+    }
+    osc_add_hook('admin_header','votingmenu');
+    
     osc_add_hook('admin_menu', 'voting_admin_menu');
 ?>
