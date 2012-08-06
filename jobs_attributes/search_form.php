06343d66d189304d85ae39eb29e26fd6ae8bbcbe
conejoninja <conejo@theninjabunny.com>
2012-08-06 16:10:46 +0200

    removed some legacy code of salary range on the search forms

--- jobs_attributes/search_form.php
+++ jobs_attributes/search_form.php
@@ -1,28 +1,3 @@
-<?php
-    $salaryRange = explode(" - ", str_replace(osc_currency(),'',Params::getParam('salaryRange') ) );
-    $salaryMin = ($salaryRange[0]!='') ? $salaryRange[0] : job_plugin_salary_min();
-    $salaryMax = (isset($salaryRange[1]) && $salaryRange[1]!='') ? $salaryRange[1] : job_plugin_salary_max();
-?>
-<style type="text/css">
-    .right .selector {
-        float: right;
-    } 
-</style>
-<script type="text/javascript">
-    $(function() {
-        $("#salary-range").slider({
-            range: true,
-            min: <?php echo job_plugin_salary_min();?>,
-            max: <?php echo job_plugin_salary_max();?>,
-            step: <?php echo job_plugin_salary_step();?>,
-            values: [<?php echo $salaryMin;?>, <?php echo $salaryMax;?>],
-            slide: function(event, ui) {
-                $("#salaryRange").val(ui.values[0] + ' <?php echo osc_currency();?> - ' + ui.values[1] + ' <?php echo osc_currency();?>');
-            }
-        });            
-        $("#salaryRange").val($("#salary-range").slider("values", 0) + ' <?php echo osc_currency();?> - ' + $("#salary-range").slider("values", 1) + ' <?php echo osc_currency();?>');
-    });
-</script>
 <fieldset>
     <h3><?php _e("Job attributes", 'jobs_attributes'); ?></h3>
     <div class="row one_input">
