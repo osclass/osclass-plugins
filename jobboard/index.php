8127408e848dbdc0c7b36caaa1f7005643a9d1c0
Juan Ramón <juanramon.diaz@gmail.com>
2012-08-10 12:19:58 +0200

    removed plugin_category in item form

--- jobboard/index.php
+++ jobboard/index.php
@@ -10,13 +10,10 @@ Short Name: jobboard_plugin
 Plugin update URI: job-board
 */
 
-require_once('ModelJB.php');
+define('JOBBOARD_PATH', dirname(__FILE__) . '/') ;
+require_once(JOBBOARD_PATH . 'ModelJB.php');
 
 function job_call_after_install() {
-    // Insert here the code you want to execute after the plugin's install
-    // for example you might want to create a table or modify some values
-	
-    // In this case we'll create a table to store the Example attributes
     ModelJB::newInstance()->import('jobboard/struct.sql');
 
     osc_set_preference('upload_path', osc_content_path()."uploads/", 'jobboard_plugin', 'STRING');
@@ -25,102 +22,78 @@ function job_call_after_install() {
 }
 
 function job_call_after_uninstall() {
-    // Insert here the code you want to execute after the plugin's uninstall
-    // for example you might want to drop/remove a table or modify some values
-
-    // In this case we'll remove the table we created to store Example attributes
     ModelJB::newInstance()->uninstall();
-    
+
     osc_delete_preference('upload_path', 'jobboard_plugin');
     osc_delete_preference('allow_cv_upload', 'jobboard_plugin');
     osc_delete_preference('version', 'jobboard_plugin');
 }
 
-function job_form($catId = null) {
-    // We received the categoryID
-    if($catId!="") {
-        // We check if the category is the same as our plugin
-        if(osc_is_this_category('jobboard_plugin', $catId)) {
-            require_once 'item_edit.php';
-        }
-    }
+/* FORM JOB BOARD */
+function jobboard_form($catID = null) {
+    require_once(JOBBOARD_PATH . 'item_edit.php');
     Session::newInstance()->_clearVariables();
 }
+osc_add_hook('item_form', 'jobboard_form');
 
-function job_form_post($catId = null, $item_id = null)  {
-    // We received the categoryID and the Item ID
-    if($catId!="") {
-        // We check if the category is the same as our plugin
-        if(osc_is_this_category('jobboard_plugin', $catId) && $item_id!=null) {
-            // Insert the data in our plugin's table
-            ModelJB::newInstance()->insertJobsAttr($item_id, Params::getParam('relation'), Params::getParam('positionType'), Params::getParam('salaryText') );
+function jobboard_form_post($catID = null, $itemID = null)  {
+    ModelJB::newInstance()->insertJobsAttr($itemID, Params::getParam('relation'), Params::getParam('positionType'), Params::getParam('salaryText') );
 
-            // prepare locales
-            $dataItem = array();
-            $request = Params::getParamsAsArray();
-            foreach ($request as $k => $v) {
-                if (preg_match('|(.+?)#(.+)|', $k, $m)) {
-                    $dataItem[$m[1]][$m[2]] = $v;
-                }
-            }
-
-            // insert locales
-            foreach ($dataItem as $k => $_data) {
-                ModelJB::newInstance()->insertJobsAttrDescription($item_id, $k, $_data['desired_exp'], $_data['studies'], $_data['min_reqs'], $_data['desired_reqs'], $_data['contract']);
-            }
+    // prepare locales
+    $dataItem = array();
+    $request = Params::getParamsAsArray();
+    foreach ($request as $k => $v) {
+        if (preg_match('|(.+?)#(.+)|', $k, $m)) {
+            $dataItem[$m[1]][$m[2]] = $v;
         }
     }
-}
 
-// Self-explanatory
+    // insert locales
+    foreach ($dataItem as $k => $_data) {
+        ModelJB::newInstance()->insertJobsAttrDescription($itemID, $k, $_data['desired_exp'], $_data['studies'], $_data['min_reqs'], $_data['desired_reqs'], $_data['contract']);
+    }
+}
+osc_add_hook('item_form_post', 'jobboard_form_post');
+
+function jobboard_item_edit($catID = null, $itemID = null) {
+    $detail       = ModelJB::newInstance()->getJobsAttrByItemId($itemID);
+    $descriptions = ModelJB::newInstance()->getJobsAttrDescriptionsByItemId($itemID);
+    $detail['locale'] = array();
+    foreach ($descriptions as $desc) {
+        $detail['locale'][$desc['fk_c_locale_code']] = $desc;
+    }
+    require_once(JOBBOARD_PATH . 'item_edit.php');
+    Session::newInstance()->_clearVariables();
+}
+osc_add_hook('item_edit', 'jobboard_item_edit');
+
+function jobboard_item_edit_post($catID = null, $itemID = null) {
+    ModelJB::newInstance()->replaceJobsAttr($itemID, Params::getParam('relation'), Params::getParam('positionType'), Params::getParam('salaryText'));
+    // prepare locales
+    $dataItem = array();
+    $request  = Params::getParamsAsArray();
+    foreach ($request as $k => $v) {
+        if (preg_match('|(.+?)#(.+)|', $k, $m)) {
+            $dataItem[$m[1]][$m[2]] = $v;
+        }
+    }
+
+    // insert locales
+    foreach ($dataItem as $k => $_data) {
+        ModelJB::newInstance()->replaceJobsAttrDescriptions($itemID, $k, $_data['desired_exp'], $_data['studies'], $_data['min_reqs'], $_data['desired_reqs'], $_data['contract']);
+    }
+}
+osc_add_hook('item_edit_post', 'jobboard_item_edit_post');
+/* /FORM JOB BOARD */
+
 function job_item_detail() {
-    if(osc_is_this_category('jobboard_plugin', osc_item_category_id())) {
-        $detail = ModelJB::newInstance()->getJobsAttrByItemId(osc_item_id());
-        $descriptions = ModelJB::newInstance()->getJobsAttrDescriptionsByItemId(osc_item_id());
-        $detail['locale'] = array();
-        foreach ($descriptions as $desc) {
-            $detail['locale'][$desc['fk_c_locale_code']] = $desc;
-        }
-        require_once 'item_detail.php';
+    $detail = ModelJB::newInstance()->getJobsAttrByItemId(osc_item_id());
+    $descriptions = ModelJB::newInstance()->getJobsAttrDescriptionsByItemId(osc_item_id());
+    $detail['locale'] = array();
+    foreach ($descriptions as $desc) {
+        $detail['locale'][$desc['fk_c_locale_code']] = $desc;
     }
-}
-
-// Self-explanatory
-function job_item_edit($catId = null, $item_id = null) {
-    if(osc_is_this_category('jobboard_plugin', $catId)) {
-        $conn = getConnection();
-        $detail = ModelJB::newInstance()->getJobsAttrByItemId($item_id);
-        $descriptions = ModelJB::newInstance()->getJobsAttrDescriptionsByItemId($item_id);
-        $detail['locale'] = array();
-        foreach ($descriptions as $desc) {
-            $detail['locale'][$desc['fk_c_locale_code']] = $desc;
-        }
-        require_once 'item_edit.php';
-    }
-    Session::newInstance()->_clearVariables();
-}
-
-function job_item_edit_post($catId = null, $item_id = null) {
-    // We received the categoryID and the Item ID
-    if($catId!=null) {
-        // We check if the category is the same as our plugin
-        if(osc_is_this_category('jobboard_plugin', $catId)) {
-            ModelJB::newInstance()->replaceJobsAttr( $item_id, Params::getParam('relation'), Params::getParam('positionType'), Params::getParam('salaryText'));
-            // prepare locales
-            $dataItem = array();
-            $request = Params::getParamsAsArray();
-            foreach ($request as $k => $v) {
-                if (preg_match('|(.+?)#(.+)|', $k, $m)) {
-                    $dataItem[$m[1]][$m[2]] = $v;
-                }
-            }
-
-            // insert locales
-            foreach ($dataItem as $k => $_data) {
-                ModelJB::newInstance()->replaceJobsAttrDescriptions( $item_id, $k, $_data['desired_exp'], $_data['studies'], $_data['min_reqs'], $_data['desired_reqs'], $_data['contract'] );
-            }
-        }
-    }
+    require_once(JOBBOARD_PATH . 'item_detail.php');
 }
 
 function job_delete_locale($locale) {
@@ -177,15 +150,7 @@ function job_js_redirect_to($url) { ?>
     </script>
 <?php }
 
-function job_admin_configuration() {
-    // Standard configuration page for plugin which extend item's attributes
-    osc_plugin_configure_view(osc_plugin_path(__FILE__) );
-}
-
-
-function job_pre_item_post()
-{
-
+function job_pre_item_post() {
     Session::newInstance()->_setForm('pj_salaryText', Params::getParam('salaryText') );
     Session::newInstance()->_setForm('pj_relation',  Params::getParam('relation') );
     Session::newInstance()->_setForm('pj_positionType',  Params::getParam('positionType') );
@@ -206,8 +171,7 @@ function job_pre_item_post()
     Session::newInstance()->_keepForm('pj_data');
 }
 
-function job_save_inputs_into_session()
-{
+function job_save_inputs_into_session() {
     Session::newInstance()->_keepForm('pj_salaryText');
     Session::newInstance()->_keepForm('pj_relation');
     Session::newInstance()->_keepForm('pj_positionType');
@@ -215,25 +179,13 @@ function job_save_inputs_into_session()
 }
 
 
-
 // this is needed in order to be able to activate the plugin
 osc_register_plugin(osc_plugin_path(__FILE__), 'job_call_after_install');
 // this is a hack to show a Uninstall link at plugins table (you could also use some other hook to show a custom option panel)
 osc_add_hook(osc_plugin_path(__FILE__)."_uninstall", 'job_call_after_uninstall');
 
-
-// when publishing an item we show an extra form with more attributes
-osc_add_hook('item_form', 'job_form');
-// to add that new information to our custom table
-osc_add_hook('item_form_post', 'job_form_post');
-
 // show an item special attributes
 osc_add_hook('item_detail', 'job_item_detail');
-
-// edit an item special attributes
-osc_add_hook('item_edit', 'job_item_edit');
-// edit an item special attributes POST
-osc_add_hook('item_edit_post', 'job_item_edit_post');
 
 // delete locale
 osc_add_hook('delete_locale', 'job_delete_locale');
