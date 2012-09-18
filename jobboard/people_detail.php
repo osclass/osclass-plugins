af6370a909abf1b92f3edf9215906b6c4e3c8aae
Juan Ramón <juanramon.diaz@gmail.com>
2012-09-18 10:45:58 +0200

    changed layout applicant view

--- jobboard/people_detail.php
+++ jobboard/people_detail.php
@@ -7,12 +7,12 @@
 
     $mjb = ModelJB::newInstance();
     $people = $mjb->getApplicant($applicantId);
-    
+
     $file = $mjb->getCVFromApplicant($applicantId);
     ModelJB::newInstance()->changeSecret($file['pk_i_id']);
     $file = $mjb->getCVFromApplicant($applicantId);
     $notes = $mjb->getNotesFromApplicant($applicantId);
-    
+
     $job = Item::newInstance()->findByPrimaryKey($people['fk_i_item_id']);
 
     if($people['b_read']==0) {
@@ -79,79 +79,76 @@
 </script>
 <div id="applicant-detail">
     <span><a href="<?php echo osc_admin_render_plugin_url("jobboard/people.php"); ?>" ><?php _e('Applicants', 'jobboard'); ?></a> &raquo; <?php echo @$people['s_name']; ?></span>
-    <div class="applicant-header" style="margin-top: 10px;">
-        <h2 class="render-title"><?php echo @$people['s_name']; ?></h2>
+    <div class="applicant-header">
+        <h2 class="render-title"><?php echo @$people['s_name']; ?> <a href="<?php echo osc_plugin_url(__FILE__); ?>download.php?data=<?php echo $applicantId; ?>|<?php echo $file['s_secret']; ?>" class="btn btn-mini" style="float:right;height:14px;"><?php _e('Download resume', 'jobboard'); ?></a></h2>
     </div>
     <div class="applicant-cover-letter">
-        <p><?php echo nl2br($people['s_cover_letter']); ?></p>
-    </div>
-    <div id="left-side">
-        <div id="dashboard_notes">
-            <?php if(empty($file)) {
-                _e("This applicant has not sumitted any resume", "jobboard");
-            } else { ?>
-            <div id="applicant-resume">
-            <iframe src="http://docs.google.com/viewer?embedded=true&url=<?php echo osc_plugin_url(__FILE__); ?>download.php?data=<?php echo $applicantId; ?>|<?php echo $file['s_secret']; ?>"></iframe>
-            </div>
-            <?php } ?>
+        <div id="left-side">
+            <h3 class="render-title"><?php _e('Cover letter', 'jobboard'); ?></h3>
+            <p><?php echo nl2br($people['s_cover_letter']); ?></p>
         </div>
-        
-    </div>
-    <div id="right-side">
-        <div class="download-resume">
-            <a href="<?php echo osc_plugin_url(__FILE__); ?>download.php?data=<?php echo $applicantId; ?>|<?php echo $file['s_secret']; ?>" class="btn"><?php _e('Download resume', 'jobboard'); ?></a>
+        <div id="right-side">
+            <div class="well ui-rounded-corners applicant-box">
+                <div class="status-icon">
+                </div>
+                <div class="rater big-star">
+                    <?php for($k=1;$k<=5;$k++) {
+                        echo '<input name="star'.$people['pk_i_id'].'" type="radio" class="auto-star required" value="'.$people['pk_i_id'].'_'.$k.'" title="'.$k.'" '.($k==$people['i_rating']?'checked="checked"':'').'/>';
+                    } ?>
+                </div>
+                <select id="applicant_status" name="applicant_status" class="select-box-medium">
+                    <?php
+                    $st_array = jobboard_status();
+                    foreach($st_array as $k => $v) {
+                        echo '<option value="'.$k.'" '.($k==$people['i_status']?'selected="selected"':'').'>'.$v.'</option>';
+                    }
+                    ?>
+                </select>
+                <div class="applied-for">
+                    <?php _e("Applied for:", "jobboard"); ?><br />
+                    <a href="<?php echo osc_item_admin_edit_url($job['fk_i_item_id']); ?>"><?php echo $job['s_title']; ?></a>
+                </div>
+            </div>
             <div style="clear:both;"></div>
-        </div>
-        <div class="well ui-rounded-corners applicant-box">
-            <div class="status-icon">
-            </div>
-            <div class="rater big-star">
-                <?php for($k=1;$k<=5;$k++) {
-                    echo '<input name="star'.$people['pk_i_id'].'" type="radio" class="auto-star required" value="'.$people['pk_i_id'].'_'.$k.'" title="'.$k.'" '.($k==$people['i_rating']?'checked="checked"':'').'/>';
-                } ?>
-            </div>
-            <select id="applicant_status" name="applicant_status" class="select-box-medium">
-                <?php
-                $st_array = jobboard_status();
-                foreach($st_array as $k => $v) {
-                    echo '<option value="'.$k.'" '.($k==$people['i_status']?'selected="selected"':'').'>'.$v.'</option>';
-                }
-                ?>
-            </select>
-            <div class="applied-for">
-                <?php _e("Applied for:", "jobboard"); ?><br />
-                <a href="<?php echo osc_item_admin_edit_url($job['fk_i_item_id']); ?>"><?php echo $job['s_title']; ?></a>
-            </div>
-        </div>
-        <h3 class="sidebar-title"><?php _e("Notes", "jobboard"); ?> <span class="note_plus"><a id="add_note" href="javascript:add_note();"><?php _e("Add note", "jobboard"); ?></a></span></h3>
-        <div id="dashboard_notes">
-            <div id="nots_table_div">
-                <?php if(count($notes)>0) { ?>
-                    <?php foreach($notes as $note) { ?>
-                        <div class="note well ui-rounded-corners">
-                            <div class="note-actions">
-                                <a class="delete_note" href="javascript:delete_note(<?php echo $note['pk_i_id']; ?>);" ><?php _e("DELETE", "jobboard"); ?></a>
-                                <a class="edit_note" href="javascript:edit_note(<?php echo $note['pk_i_id']; ?>, '<?php echo osc_esc_js($note['s_text']); ?>');" ><?php _e("EDIT", "jobboard"); ?></a>
-                            </div>
-                            <div class="note-date">
-                                <b><?php echo date('d', strtotime($note['dt_date'])); ?></b>
-                                <span><?php echo date('M', strtotime($note['dt_date'])); ?><br>
-                                <?php echo date('Y', strtotime($note['dt_date'])); ?></span>
-                            </div>
-                            <div class="clear"></div>
-                            <p><?php echo $note['s_text']; ?></p>
-                        </div>
-                    <?php }; ?>
-                <?php } else { ?>
-                    <div class="well ui-rounded-corners">
-                    <?php _e("No notes have been added to this applicant", "jobboard"); ?>
-                    </div>
-                <?php }; ?>
-            </div>
         </div>
         <div style="clear:both;"></div>
     </div>
+    <div id="applicant-resume">
+        <?php if(empty($file)) {
+            _e("This applicant has not sumitted any resume", "jobboard");
+        } else { ?>
+        <iframe src="http://docs.google.com/viewer?embedded=true&url=<?php echo str_replace("localhost", "95.62.72.123", osc_plugin_url(__FILE__));?>download.php?data=<?php echo $applicantId; ?>|<?php echo $file['s_secret']; ?>"></iframe>
+        <?php } ?>
+    </div>
+    <h3 class="sidebar-title render-title">
+        <?php _e("Notes", "jobboard"); ?> <span class="note_plus"><a id="add_note" class="btn btn-mini" href="javascript:add_note();"><?php _e("Add note", "jobboard"); ?></a></span>
+    </h3>
     <div style="clear:both;"></div>
+    <div id="dashboard_notes">
+        <div id="nots_table_div">
+            <?php if(count($notes)>0) { ?>
+                <?php foreach($notes as $note) { ?>
+                    <div class="note well ui-rounded-corners">
+                        <div class="note-actions">
+                            <a class="delete_note" href="javascript:delete_note(<?php echo $note['pk_i_id']; ?>);" ><?php _e("DELETE", "jobboard"); ?></a>
+                            <a class="edit_note" href="javascript:edit_note(<?php echo $note['pk_i_id']; ?>, '<?php echo osc_esc_js($note['s_text']); ?>');" ><?php _e("EDIT", "jobboard"); ?></a>
+                        </div>
+                        <div class="note-date">
+                            <b><?php echo date('d', strtotime($note['dt_date'])); ?></b>
+                            <span><?php echo date('M', strtotime($note['dt_date'])); ?><br>
+                            <?php echo date('Y', strtotime($note['dt_date'])); ?></span>
+                        </div>
+                        <div class="clear"></div>
+                        <p><?php echo $note['s_text']; ?></p>
+                    </div>
+                <?php }; ?>
+            <?php } else { ?>
+                <div class="note well ui-rounded-corners">
+                <?php _e("No notes have been added to this applicant", "jobboard"); ?>
+                </div>
+            <?php } ?>
+        </div>
+    </div>
 </div>
 <form id="dialog-note-delete" method="post" action="<?php echo osc_admin_base_url(true); ?>" class="has-form-actions hide" title="<?php echo osc_esc_html(__('Delete note', 'jobboard')); ?>">
     <input type="hidden" name="page" value="plugins" />
