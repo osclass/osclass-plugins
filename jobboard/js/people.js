a630ffa6c697381e3c0883f6dad263b8bfedb27e
Carlos <garciademarina@gmail.com>
2012-10-02 20:03:58 +0200

    added linkedin ico, added btn show form filters

--- jobboard/js/people.js
+++ jobboard/js/people.js
@@ -1,6 +1,6 @@
-$(document).ready(function(){
-    $('.list-star').rating({showCancel: false, readOnly: true});
+$(document).ready(function() {
 
+    $('.list-star').rating({showCancel: false, readOnly: true});
     $('.filter-star').rating({showCancel: true, cancelValue: null});
 
     $("#dialog-people-delete").dialog({
@@ -10,8 +10,22 @@ $(document).ready(function(){
 
     // tooltips notes
     $.each($('.note'), function(index, value) {
+        $(value).osc_tooltip($(value).attr('data-tooltip'), {layout: 'gray-tooltip', position: {x: 'right', y: 'middle'}});
+    });
+
+    $('#show-filters').click( function(){
+        $('.search-filter').toggle();
+        if( $('.search-filter:visible').size() > 0 ) {
+            $('#show-filters').text( jobboard.text_hide_filter );
+        } else {
+            $('#show-filters').text( jobboard.text_show_filter );
+        }
+    });
+    // tooltips notes
+    $.each($('.note'), function(index, value) {
         $(value).osc_tooltip($(value).attr('data-tooltip'), {layout: 'gray-tooltip', position: { x: 'right', y: 'middle' }});
     });
+
 })
 
 function delete_applicant(id) {
