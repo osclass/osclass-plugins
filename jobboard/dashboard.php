bf0da48ac5486772c49ac2fddd5a15ee2dd0476c
Carlos <garciademarina@gmail.com>
2012-10-08 16:40:20 +0200

    dashboard added Job title when apply to Spontaneous application

--- jobboard/dashboard.php
+++ jobboard/dashboard.php
@@ -140,7 +140,11 @@
                                     }
                                     echo '<tr>';
                                     echo '<td><a href="'.osc_admin_render_plugin_url("jobboard/people_detail.php").'&people='.$applicant['pk_i_id'].'">'.$applicant['s_name']; if($applicant['b_has_notes'] == 1 ) { echo '<span class="note" data-tooltip="'.$note_tooltip.'"></span>'; } echo '</a></td>';
-                                    echo '<td>'.osc_highlight(@$item['s_title'],30).'</td>';
+                                    if( !is_null(@$applicant['fk_i_item_id']) ) {
+                                        echo '<td>'.osc_highlight(@$item['s_title'],30).'</td>';
+                                    } else {
+                                        echo '<td>'.__('Spontaneous application', 'jobboard').'</td>';
+                                    }
                                     echo '<td>'._jobboard_time_elapsed_string( strtotime(@$applicant['dt_date']) ) .'</td>';
                                     echo '</tr>';
                                 }
