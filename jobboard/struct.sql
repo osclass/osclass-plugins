37125beea54116a9ac01443eddb8925b6040dea7
Carlos <garciademarina@gmail.com>
2012-10-18 16:10:30 +0200

    added killer questions database structure and added foreign keys to JobsAttr and JobsApplicants

--- jobboard/struct.sql
+++ jobboard/struct.sql
@@ -1,9 +1,65 @@
+/* killer questions related */
+
+CREATE TABLE /*TABLE_PREFIX*/t_question (
+  pk_i_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  e_type ENUM('CLOSED', 'OPENED') NOT NULL,
+  s_text TINYTEXT NOT NULL,
+
+    PRIMARY KEY (pk_i_id)
+) ENGINE=InnoDB DEFAULT CHARACTER SET 'UTF8' COLLATE 'UTF8_GENERAL_CI';
+
+CREATE TABLE /*TABLE_PREFIX*/t_answer (
+  pk_i_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  fk_i_question_id INT UNSIGNED NULL,
+  s_text TINYTEXT NOT NULL,
+  s_punctuation VARCHAR(45) NULL DEFAULT NULL,
+  b_reject TINYINT NOT NULL DEFAULT 0,
+
+  PRIMARY KEY (pk_i_id),
+  FOREIGN KEY (fk_i_question_id) REFERENCES /*TABLE_PREFIX*/t_question (pk_i_id)
+) ENGINE=InnoDB DEFAULT CHARACTER SET 'UTF8' COLLATE 'UTF8_GENERAL_CI';
+
+CREATE TABLE /*TABLE_PREFIX*/t_killer_form (
+  pk_i_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  s_title VARCHAR(255) NOT NULL DEFAULT '',
+  dt_pub_date DATETIME NOT NULL,
+  dt_mod_date DATETIME NULL DEFAULT NULL,
+
+    PRIMARY KEY (pk_i_id)
+) ENGINE=InnoDB DEFAULT CHARACTER SET 'UTF8' COLLATE 'UTF8_GENERAL_CI';
+
+CREATE TABLE /*TABLE_PREFIX*/t_killer_form_questions (
+  fk_i_killer_form_id INT UNSIGNED NULL,
+  fk_i_question_id INT UNSIGNED NULL,
+  i_order INT NOT NULL,
+
+    PRIMARY KEY (fk_i_killer_form_id, fk_i_question_id),
+    FOREIGN KEY (fk_i_killer_form_id) REFERENCES /*TABLE_PREFIX*/t_killer_form (pk_i_id),
+    FOREIGN KEY (fk_i_question_id) REFERENCES /*TABLE_PREFIX*/t_question (pk_i_id)
+) ENGINE=InnoDB DEFAULT CHARACTER SET 'UTF8' COLLATE 'UTF8_GENERAL_CI';
+
+CREATE TABLE /*TABLE_PREFIX*/t_killer_form_results (
+  fk_i_applicant_id INT UNSIGNED NULL,
+  fk_i_killer_form_id INT UNSIGNED NULL,
+  fk_i_question_id INT UNSIGNED NULL,
+  fk_i_answer_id INT UNSIGNED NOT NULL,
+  s_answer_opened TINYTEXT NULL DEFAULT NULL,
+
+    PRIMARY KEY (fk_i_applicant_id, fk_i_killer_form_id, fk_i_question_id),
+    FOREIGN KEY (fk_i_killer_form_id) REFERENCES /*TABLE_PREFIX*/t_killer_form (pk_i_id),
+    FOREIGN KEY (fk_i_question_id) REFERENCES /*TABLE_PREFIX*/t_question (pk_i_id),
+    FOREIGN KEY (fk_i_answer_id) REFERENCES /*TABLE_PREFIX*/t_answer (pk_i_id)
+) ENGINE=InnoDB DEFAULT CHARACTER SET 'UTF8' COLLATE 'UTF8_GENERAL_CI';
+
+/* jobboard */
+
 CREATE TABLE /*TABLE_PREFIX*/t_item_job_attr (
     fk_i_item_id INT UNSIGNED NOT NULL,
     e_position_type ENUM('UNDEF', 'PART', 'FULL'),
     s_salary_text TEXT NOT NULL DEFAULT '',
+    fk_i_killer_form_id INT UNSIGNED NULL,
         PRIMARY KEY (fk_i_item_id),
-        FOREIGN KEY (fk_i_item_id) REFERENCES /*TABLE_PREFIX*/t_item (pk_i_id)
+        FOREIGN KEY (fk_i_killer_form_id) REFERENCES /*TABLE_PREFIX*/t_killer_form (pk_i_id)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET 'UTF8' COLLATE 'UTF8_GENERAL_CI';
 
 CREATE TABLE /*TABLE_PREFIX*/t_item_job_description_attr (
@@ -35,6 +91,7 @@ CREATE TABLE /*TABLE_PREFIX*/t_item_job_applicant (
     s_ip VARCHAR(15) NOT NULL DEFAULT '',
     s_sex VARCHAR(15) NOT NULL DEFAULT 'prefernotsay',
     dt_birthday DATE NOT NULL DEFAULT '0000-00-00',
+
         PRIMARY KEY (pk_i_id)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET 'UTF8' COLLATE 'UTF8_GENERAL_CI';
 
@@ -59,3 +116,4 @@ CREATE TABLE /*TABLE_PREFIX*/t_item_job_note (
         PRIMARY KEY (pk_i_id),
         FOREIGN KEY (fk_i_applicant_id) REFERENCES /*TABLE_PREFIX*/t_item_job_applicant (pk_i_id)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET 'UTF8' COLLATE 'UTF8_GENERAL_CI';
+
