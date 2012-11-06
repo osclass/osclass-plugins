8593ecb516a1a6f5bf30428ae4fec756db9d127c
Carlos <garciademarina@gmail.com>
2012-11-06 16:45:53 +0100

    added delete_killer_form case

--- jobboard/actions.php
+++ jobboard/actions.php
@@ -9,6 +9,15 @@
             ModelJB::newInstance()->deleteApplicant(Params::getParam("id"));
             job_js_redirect_to(osc_admin_render_plugin_url("jobboard/people.php"));
         break;
+        case 'delete_killer_form':
+            $result = ModelKQ::newInstance()->removeKillerForm(Params::getParam("id"));
+            if($result!==false) {
+                osc_add_flash_ok_message(__('Killer form deleted correctly', 'jobboard'), 'admin');
+            } else {
+                osc_add_flash_message(__('Problems tring to remove killer question form. Try it again.', 'jobboard'), 'admin');
+            }
+            job_js_redirect_to(osc_admin_render_plugin_url("jobboard/manage_killer.php"));
+        break;
         default :
             job_js_redirect_to(osc_admin_render_plugin_url("jobboard/dashboard.php"));
         break;
