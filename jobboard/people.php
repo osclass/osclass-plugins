beeb51c688992235556b8f03107e0f66462d8145
Carlos <garciademarina@gmail.com>
2012-11-08 12:21:26 +0100

    minor fixes, table colspan fixed

--- jobboard/people.php
+++ jobboard/people.php
@@ -331,7 +331,7 @@
                 <?php } ?>
                 <?php } else { ?>
                 <tr>
-                    <td colspan="8" class="text-center">
+                    <td colspan="9" class="text-center">
                         <p><?php _e('No data available in table', 'jobboard') ; ?></p>
                     </td>
                 </tr>
