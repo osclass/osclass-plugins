5e741507cf88df92ddc8848b9aca359f0d7c4208
Carlos <garciademarina@gmail.com>
2012-11-28 10:44:43 +0100

    fixed menus issues, bad conflict resolved related

--- jobboard/index.php
+++ jobboard/index.php
@@ -957,6 +957,22 @@ osc_add_admin_submenu_page(
     'moderator'
 );
 
+osc_add_admin_submenu_page(
+     'jobboard',
+     __('Download resumes', 'jobboard'),
+     osc_admin_render_plugin_url("jobboard/resume_download.php"),
+     'jobboard_resumedownload',
+     'moderator'
+);
+
+osc_add_admin_submenu_page(
+    'jobboard',
+    __('Default locations', 'jobboard'),
+    osc_admin_render_plugin_url("jobboard/admin/settings.php"),
+    'jobboard_locations',
+    'moderator'
+);
+
 function jobboard_rating($applicantId, $rating = 0) {
     $str = '<span class="rating" id="rating_'.$applicantId.'" rating="'.$rating.'">';
     for($k=1;$k<=5;$k++) {
@@ -1501,45 +1517,45 @@ if( $subdomain == 'osclass.com') {
     osc_add_hook('init', 'jobboard_set_domain_linkedin');
 }
 
-osc_add_admin_menu_page(
-    __('Jobboard', 'jobboard'),
-    '#',
-    'jobboard',
-    'moderator'
-);
-
-osc_add_admin_submenu_page(
-    'jobboard',
-    __('Dashboard', 'jobboard'),
-    osc_admin_render_plugin_url("jobboard/dashboard.php"),
-    'jobboard_dash',
-    'moderator'
-);
-
-osc_add_admin_submenu_page(
-    'jobboard',
-    __('Applicants', 'jobboard'),
-    osc_admin_render_plugin_url("jobboard/people.php"),
-    'jobboard_people',
-    'moderator'
-);
-
-// killer questions menu
-osc_add_admin_submenu_page(
-    'jobboard',
-    __('Killer Questions', 'jobboard'),
-    osc_admin_render_plugin_url("jobboard/manage_killer.php"),
-    'jobboard_killer',
-    'moderator'
-);
-
-osc_add_admin_submenu_page(
-    'jobboard',
-    __('Default locations', 'jobboard'),
-    osc_admin_render_plugin_url("jobboard/admin/settings.php"),
-    'jobboard_locations',
-    'moderator'
-);
+//osc_add_admin_menu_page(
+//    __('Jobboard', 'jobboard'),
+//    '#',
+//    'jobboard',
+//    'moderator'
+//);
+//
+//osc_add_admin_submenu_page(
+//    'jobboard',
+//    __('Dashboard', 'jobboard'),
+//    osc_admin_render_plugin_url("jobboard/dashboard.php"),
+//    'jobboard_dash',
+//    'moderator'
+//);
+//
+//osc_add_admin_submenu_page(
+//    'jobboard',
+//    __('Applicants', 'jobboard'),
+//    osc_admin_render_plugin_url("jobboard/people.php"),
+//    'jobboard_people',
+//    'moderator'
+//);
+//
+//// killer questions menu
+//osc_add_admin_submenu_page(
+//    'jobboard',
+//    __('Killer Questions', 'jobboard'),
+//    osc_admin_render_plugin_url("jobboard/manage_killer.php"),
+//    'jobboard_killer',
+//    'moderator'
+//);
+//
+//osc_add_admin_submenu_page(
+//    'jobboard',
+//    __('Default locations', 'jobboard'),
+//    osc_admin_render_plugin_url("jobboard/admin/settings.php"),
+//    'jobboard_locations',
+//    'moderator'
+//);
 
 /**
  * Used by exting questions-answers
