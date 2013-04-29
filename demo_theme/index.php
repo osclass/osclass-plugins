e456d8aec6ea716674bf64a162e9a66b21815958
Carlos <garciademarina@gmail.com>
2013-04-29 16:24:09 +0200

    added osclass logo, ocadmin link and style improvements, minor fixes

--- demo_theme/index.php
+++ demo_theme/index.php
@@ -13,34 +13,36 @@ Plugin update URI: demo-theme
     /**
      * Set the theme is going to be loaded. It can be passed by parameter or get from the Cookie
      */
-    function change_theme() {
-        $theme = '' ;
-        // check first if it has been set in the cookie
-        if( Cookie::newInstance()->get_value('demo_theme') != '' ) {
-            $theme = Cookie::newInstance()->get_value('demo_theme') ;
-        }
-
-        if( Params::getParam('theme') != '' ) {
-            $theme = Params::getParam('theme') ;
-            Cookie::newInstance()->set_expires( 86400 * 30 ) ;
-            Cookie::newInstance()->push('demo_theme', $theme) ;
-            Cookie::newInstance()->set() ;
-        }
-
-        if( $theme == '' ) {
-            return false ;
-        }
-
-        $theme_path = osc_themes_path() . $theme . '/';
-        if( file_exists($theme_path) ) {
-            WebThemes::newInstance()->setCurrentTheme($theme) ;
-            $functions_path = WebThemes::newInstance()->getCurrentThemePath() . 'functions.php';
-            if ( file_exists($functions_path) ) {
-                require_once $functions_path ;
+    if( !function_exists('change_theme') ) {
+        function change_theme() {
+            $theme = '' ;
+            // check first if it has been set in the cookie
+            if( Cookie::newInstance()->get_value('demo_theme') != '' ) {
+                $theme = Cookie::newInstance()->get_value('demo_theme') ;
             }
-        }
 
-        return true ;
+            if( Params::getParam('theme') != '' ) {
+                $theme = Params::getParam('theme') ;
+                Cookie::newInstance()->set_expires( 86400 * 30 ) ;
+                Cookie::newInstance()->push('demo_theme', $theme) ;
+                Cookie::newInstance()->set() ;
+            }
+
+            if( $theme == '' ) {
+                return false ;
+            }
+
+            $theme_path = osc_themes_path() . $theme . '/';
+            if( file_exists($theme_path) ) {
+                WebThemes::newInstance()->setCurrentTheme($theme) ;
+                $functions_path = WebThemes::newInstance()->getCurrentThemePath() . 'functions.php';
+                if ( file_exists($functions_path) ) {
+                    require_once $functions_path ;
+                }
+            }
+
+            return true ;
+        }
     }
 
     /**
@@ -55,7 +57,7 @@ Plugin update URI: demo-theme
      *     - Theme via parameter
      *     - Theme via Cookie
      *     - Default theme
-     * 
+     *
      * @return string The name of the selected theme
      */
     function selected_theme() {
@@ -96,13 +98,45 @@ Plugin update URI: demo-theme
         }
         echo ' } ;' . PHP_EOL;
         echo '        $("body").prepend( $("<div>").attr("id", "theme_header") ) ;' . PHP_EOL ;
-        echo '        $("#theme_header").append( $("<div>").attr("id", "theme_selector") ) ;' . PHP_EOL ;
-        echo '        $("#theme_selector").append( $("<label>").html("' . __('Choose a theme', 'demo_theme') . '") ) ;' . PHP_EOL ;
-        echo '        $("#theme_selector").append( $("<div>").attr("class", "select") ) ;' . PHP_EOL ;
-        echo '        $("#theme_selector .select").append( $("<select>").attr("id", "select_theme") ) ;' . PHP_EOL ;
+
+
+
+
+        // wrapper_demo
+        echo '        var wrapper = $("<div>").attr("class", "wrapper_demo") ;' . PHP_EOL ;
+
+        // logo
+        echo '        var part_logo = $("<div>").attr("class", "logo_demo") ;' . PHP_EOL ;
+        echo '        var img_logo = $("<img>").attr("src", "'.osc_base_url().'oc-content/plugins/demo_theme/default-logo.png") ;' . PHP_EOL ;
+        echo '        $(part_logo).prepend(img_logo) ;' . PHP_EOL ;
+
+        // oc-admin link
+        echo '        var part_ocadmin = $("<div>").attr("class", "ocadmin_demo") ;' . PHP_EOL ;
+        echo '        var link_ocadmin = $("<a>").attr("href", "'.osc_admin_base_url(true).'") ;' . PHP_EOL ;
+        echo '        $(link_ocadmin).attr("class", "button-link") ;' . PHP_EOL ;
+        echo '        $(link_ocadmin).text("'.__('Admin panel', 'demo_theme').'") ;' . PHP_EOL ;
+        echo '        $(part_ocadmin).prepend(link_ocadmin) ;' . PHP_EOL ;
+
+        echo '        var part_selector = $("<div>").attr("id", "theme_selector") ;' . PHP_EOL ;
+
+        echo '        $(part_selector).append( $("<label>").html("' . __('Choose a theme', 'demo_theme') . '") ) ;' . PHP_EOL ;
+        echo '        $(part_selector).append( $("<div>").attr("class", "select") ) ;' . PHP_EOL ;
+
+        echo '        $(wrapper).append(part_logo) ;' . PHP_EOL ;
+        echo '        $(wrapper).append( part_selector ) ;' . PHP_EOL ;
+        echo '        $(wrapper).append( part_ocadmin ) ;' . PHP_EOL ;
+        echo '        $(wrapper).append( $("<div>").attr("class", "clear") ) ;' . PHP_EOL ;
+
+        echo '        $("#theme_header").append(wrapper) ; ';
+        echo '        $("#theme_header").after( $("<div>").attr("class", "clear") ) ;' . PHP_EOL ;
+
+        echo '        var select = $("<select>").attr("id", "select_theme") ; ';
+        echo '        $(part_selector).find(".select").append( select ) ;' . PHP_EOL ;
+
         echo '        $.each(theme, function(key, value) {' . PHP_EOL ;
         echo '            $("#select_theme").prepend( $("<option>").html(value).attr("value", key) ) ;' . PHP_EOL ;
         echo '        }) ;' . PHP_EOL ;
+
         echo '        $("#select_theme option[value=\'' . $selected_theme .'\']").attr(\'selected\', \'selected\') ;' . PHP_EOL ;
         echo '        $("#select_theme").change(function () {' . PHP_EOL ;
         echo "            url = window.location.href.replace(/[\?&]theme=[\w]+/, '') ;" . PHP_EOL ;
@@ -116,10 +150,13 @@ Plugin update URI: demo-theme
         if( @$info_selected_theme['theme_update_uri'] != "") {
             echo "        $('#theme_selector .select').append( $('<span>').html( $('<a>').attr('href', 'http://market.osclass.org/api/view/" . @$info_selected_theme['theme_update_uri'] . "').attr('target', '_blank').html('Download " . $info_selected_theme['name'] . " theme') ) ) ; " . PHP_EOL ;
         }
+
+
+
         echo '        $("#theme_header").append( $("<div>").css("clear:both;") ) ;' ;
         echo '    }) ;' . PHP_EOL ;
         echo '</script>' . PHP_EOL ;
-        
+
     }
 
     osc_add_hook('before_html', 'change_theme') ;
