76099f53ab7c3d1f8c52d8a4a422e79059f91426
Carlos <garciademarina@gmail.com>
2013-05-23 11:05:48 +0200

    minor improvements

--- jobboard/class/JobboardCustomTitles.php
+++ jobboard/class/JobboardCustomTitles.php
@@ -7,18 +7,21 @@ class JobboardCustomTitles
         osc_add_filter('custom_plugin_title',   array(&$this, 'jobboard_title'));
     }
 
-    function jobboard_title($string){
-        if(Params::getParam('page') == 'plugins' && Params::getParam('file') == 'jobboard/people_detail.php'){
-            $string = __('Applicant', 'jobboard') . '<a href="#" class="btn ico ico-32 ico-help float-right"></a>';
-        }
-        if(Params::getParam('page') == 'plugins' && Params::getParam('file') == 'jobboard/dashboard.php'){
-            $string = __('Dashboard', 'jobboard');
-        }
-        if(Params::getParam('page') == 'plugins' && Params::getParam('file') == 'jobboard/resume_download.php'){
-            $string = __('Download resumes', 'jobboard');
-        }
-        if(Params::getParam('page') == 'plugins' && Params::getParam('file') == 'jobboard/people.php'){
-            $string = __('Applicants', 'jobboard') . '<a href="#" class="btn ico ico-32 ico-help float-right"></a>';
+    function jobboard_title($string)
+    {
+        if(Params::getParam('page') == 'plugins') {
+            if(Params::getParam('file') == 'jobboard/people_detail.php') {
+                $string = __('Applicant', 'jobboard') . '<a href="#" class="btn ico ico-32 ico-help float-right"></a>';
+            }
+            if(Params::getParam('file') == 'jobboard/dashboard.php') {
+                $string = __('Dashboard', 'jobboard');
+            }
+            if(Params::getParam('file') == 'jobboard/resume_download.php') {
+                $string = __('Download resumes', 'jobboard');
+            }
+            if(Params::getParam('file') == 'jobboard/people.php') {
+                $string = __('Applicants', 'jobboard') . '<a href="#" class="btn ico ico-32 ico-help float-right"></a>';
+            }
         }
         return $string;
     }
