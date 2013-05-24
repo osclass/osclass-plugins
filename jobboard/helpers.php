a95a149c3288fb4e09681d9209ad2a3603fb1fd5
Carlos <garciademarina@gmail.com>
2013-05-24 18:11:53 +0200

    moved gettext filters to helpers.php

--- jobboard/helpers.php
+++ jobboard/helpers.php
@@ -1,4 +1,16 @@
 <?php
+/*
+ * gettext filter
+ */
+function jobboard_replace_listing($string) {
+    return preg_replace(array('|Listing|', '|listing|'), array(__('Vacancy', 'jobboard'), __('vacancy', 'jobboard')), $string);
+}
+osc_add_filter('gettext', 'jobboard_replace_listing');
+function jobboard_replace_listing_plural($string) {
+    return preg_replace(array('|Listings|', '|listings|'), array(__('Vacancies', 'jobboard'), __('vacancies', 'jobboard')), $string);
+}
+osc_add_filter('gettext', 'jobboard_replace_listing_plural', 1);
+
 
 if(!function_exists('jobboard_attributes_array')) {
     function jobboard_attributes_array(){
@@ -22,6 +34,71 @@ function job_js_redirect_to($url) { ?>
         window.location = "<?php echo $url; ?>"
     </script>
 <?php }
+
+/**
+ * Show dropdown punctuation in html code
+ *
+ * @param type $questionId
+ * @param type $answerId
+ * @param type $default
+ */
+function _punctuationSelect_insert( $questionId, $answerId, $default = '', $disabled = false) {
+    _punctuationSelect(true , $questionId, $answerId, $default, $disabled);
+}
+function _punctuationSelect_update( $questionId, $answerId, $default = '', $disabled = false) {
+    _punctuationSelect(false, $questionId, $answerId, $default, $disabled);
+}
+function _punctuationSelect($new, $questionId, $answerId, $default = '', $disabled = false) {
+    $aux = 'answer_punct';
+    if($new) {
+        $aux = 'new_answer_punct';
+    }
+    ?><select <?php if($disabled){ ?>disabled="disabled"<?php } ?>name="question[<?php echo $questionId;?>][<?php echo $aux;?>][<?php echo $answerId;?>]" class="select-box-medium">
+        <option value="" <?php if($default==''){ echo 'selected'; } ?>><?php _e('Punctuation', 'jobboard'); ?></option>
+        <option value="10" <?php if($default=='10'){ echo 'selected'; } ?>>10</option>
+        <option value="9" <?php if($default=='9'){ echo 'selected'; } ?>>9</option>
+        <option value="8" <?php if($default=='8'){ echo 'selected'; } ?>>8</option>
+        <option value="7" <?php if($default=='7'){ echo 'selected'; } ?>>7</option>
+        <option value="6" <?php if($default=='6'){ echo 'selected'; } ?>>6</option>
+        <option value="5" <?php if($default=='5'){ echo 'selected'; } ?>>5</option>
+        <option value="4" <?php if($default=='4'){ echo 'selected'; } ?>>4</option>
+        <option value="3" <?php if($default=='3'){ echo 'selected'; } ?>>3</option>
+        <option value="2" <?php if($default=='2'){ echo 'selected'; } ?>>2</option>
+        <option value="1" <?php if($default=='1'){ echo 'selected'; } ?>>1</option>
+        <option value="reject" <?php if($default=='reject'){ echo 'selected'; } ?>><?php _e('Reject', 'jobboard'); ?></option>
+    </select>
+    <?php
+}
+
+/**
+ * Return rating html code given a rating to represent.
+ *
+ * @param type $applicantId
+ * @param type $rating
+ * @return string
+ */
+function jobboard_rating($applicantId, $rating = 0) {
+    $str = '<span class="rating" id="rating_'.$applicantId.'" rating="'.$rating.'">';
+    for($k=1;$k<=5;$k++) {
+        $str .= '<a href="#" class="star" star="'.$k.'" id="rating_'.$applicantId.'_'.$k.'" ><img src="'.osc_base_url().'oc-content/plugins/'.osc_plugin_folder(__FILE__).'img/'.($k<=$rating?'fullstar.png':'emptystar.png').'"/></a>';
+    }
+    $str .= '</span>';
+    return $str;
+}
+
+/**
+ * Return array with posible status
+ *
+ * @return type
+ */
+function jobboard_status() {
+    $status_array = array();
+    $status_array[0] = __("Active", "jobboard");
+    $status_array[1] = __("Interview", "jobboard");
+    $status_array[2] = __("Rejected", "jobboard");
+    $status_array[3] = __("Hired", "jobboard");
+    return $status_array;
+}
 
 /**
  * Return sex string given input value
