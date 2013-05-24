78f3e166cf8d2f17535434f2802d745fab5eb84d
Carlos <garciademarina@gmail.com>
2013-05-24 18:13:36 +0200

    added class ManageListings, modifing the normal behaviour

---
+++ jobboard/class/JobboardManageListings.php
@@ -0,0 +1,65 @@
+<?php
+
+/*
+ * Modify Manage Listings/Vacancies at oc-admin
+ */
+class JobboardManageListings
+{
+    public function __construct() {
+        // modify manage listings table header
+        osc_add_hook('admin_items_table',       array(&$this, 'job_items_table_header') );
+        osc_add_filter("items_processing_row",  array(&$this, "job_items_row") );
+        // modify actions/more-actions (manage listing row)
+        osc_add_filter('more_actions_manage_items', array(&$this, 'jobboard_more_options') );
+        osc_add_filter('actions_manage_items',      array(&$this, 'jobboard_manage_actions') );
+    }
+
+    /**
+     * Modify table with custom header
+     * @param type $table
+     */
+    function job_items_table_header($table) {
+        $table->addColumn("mod_date", __("Modified", "jobboard"));
+        $table->addColumn("applicants", __("# of applicants", "jobboard"));
+        $table->addColumn("views", __("Views", "jobboard"));
+        $table->removeColumn("user");
+        $table->removeColumn("category");
+    }
+
+
+    /**
+     * Add new fields to the table
+     *
+     * @param type $row
+     * @param type $aRow
+     * @return type
+     */
+    function job_items_row($row, $aRow) {
+        list($applicants, $total) = ModelJB::newInstance()->searchCount(array('item' => $aRow['pk_i_id']));
+
+        $row['mod_date'] = @$aRow['dt_mod_date'];
+        $row['applicants'] = '<a href="' . osc_admin_render_plugin_url("jobboard/people.php&jobId=") . $aRow['pk_i_id'] . '">' . sprintf(__('%d applicants', 'jobboard'), $applicants) . '</a>';
+        $views = 0;
+        if( @$aRow['i_num_views'] > 0 ) {
+            $views = $aRow['i_num_views'];
+        }
+        $row['views'] = @$aRow['i_num_views'];
+        return $row;
+    }
+
+    function jobboard_more_options($options, $aRow) {
+        return array();
+    }
+
+    function jobboard_manage_actions($options, $aRow) {
+        if($aRow['b_enabled']) {
+            $options[] = '<a href="' . osc_admin_base_url(true) . '?page=items&amp;action=status&amp;id=' . $aRow['pk_i_id'] . '&amp;value=DISABLE">' . __('Block', 'jobboard') .'</a>' ;
+        } else {
+            $options[] = '<a href="' . osc_admin_base_url(true) . '?page=items&amp;action=status&amp;id=' . $aRow['pk_i_id'] . '&amp;value=ENABLE">' . __('Unblock', 'jobboard') .'</a>' ;
+        }
+        $options[] = '<a href="' . osc_admin_base_url(true) . '?page=items&amp;action=post&amp;duplicatefrom=' . $aRow['pk_i_id'] . '">' . __('Duplicate', 'jobboard') . '</a>' ;
+        return $options;
+    }
+
+
+}
