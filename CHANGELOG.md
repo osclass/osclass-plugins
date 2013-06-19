5904e8701b5e78b10a7f2c83d8d47cefe8803875
conejoninja <conejo@theninjabunny.com>
2013-06-19 13:29:32 +0200

    updated to work with Osclass 3.1+

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 #Changelog Location required
 
+##version 1.0.4 - 19/06/2013
+
+* Updated to work with 3.1+
+
 ##version 1.0 - 15/09/2011
 
 * First version of the plugin
--- location_required/index.php
+++ location_required/index.php
@@ -3,18 +3,16 @@
 Plugin Name: Location required
 Plugin URI: http://www.osclass.org/
 Description: This Plugin makes the location required when posting or editing ads.
-Version: 1.0.2
-Author: JChapman
+Version: 1.0.4
+Author: JChapman & Osclass
 Author URI: http://www.osclass.org/
 Short Name: location_required
 Plugin update URI: location-required
 */
 
-function metadata_js() {
-    ?>
-    <script type="text/javascript" src="<?php echo osc_base_url() . 'oc-content/plugins/location_required/jquery.metadata.js' ; ?>"></script>
-    <?php
-}
+
+osc_register_script('jquery-metadata', osc_base_url().'oc-content/plugins/location_required/jquery.metadata.js', array('jquery'));
+osc_enqueue_script('jquery-metadata');
 
 function location_js() {
 	?>
@@ -35,8 +33,6 @@ function location_js() {
     osc_register_plugin(osc_plugin_path(__FILE__), '') ;
     // This is a hack to show a Uninstall link at plugins table (you could also use some other hook to show a custom option panel)
     osc_add_hook(osc_plugin_path(__FILE__) . '_uninstall', '') ;
-
-    osc_add_hook('header', 'metadata_js') ;
 
     osc_add_hook('footer', 'location_js') ;
 
